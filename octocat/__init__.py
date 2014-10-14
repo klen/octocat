d7a8dc54ad7482ed36734d54592d985c18c77645
Kirill Klenov <horneds@gmail.com>
2014-10-14 15:41:43 +0400

    Bump version: 0.1.3 → 0.2.0

--- octocat/__init__.py
+++ octocat/__init__.py
@@ -3,7 +3,7 @@
 # Package information
 # ===================
 
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 __project__ = "octocat"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
